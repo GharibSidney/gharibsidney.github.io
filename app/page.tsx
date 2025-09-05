e9e1c2b3f0bee18a662801567dc1b2398c3f3c5f
GharibSidney <sidney-gharib@hotmail.com>
2025-09-05 15:59:35 -0400

    added 2 more components

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/src/components/ui/avatar"
 import { Button } from "@/src/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/src/components/ui/card";
 import { Linkedin, FileText, Github, Mail, MapPin, Calendar } from "lucide-react";
+import { FloatingDockDemo } from "@/src/components/ui/FloatingDockDemo";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground dark">
       <div className="container mx-auto px-4 py-8 max-w-4xl">
         {/* Hero Section */}
-        <div className="flex flex-col md:flex-row items-center gap-8 mb-12">
+        <div className="flex flex-col md:flex-row items-center gap-8 mb-10">
           {/* Profile Image */}
           <div className="flex-shrink-0 fade-in-bottom">
             <Avatar className="w-48 h-48">
@@ -26,11 +27,13 @@ export default function Home() {
             <p className="mb-6 text-muted-foreground fade-in-bottom">
               I just graduated from Polytechnique Montréal with a bachelor in Software Engineering.
               I am a passionate software developer and researcher with expertise in full-stack development,
-              machine learning, and Computer Vision. I enjoy building applications
+              machine learning, and computer vision. I enjoy building applications
               that solve real-world problems and contributing to the scientific community through
               research and publications.
             </p>
-            <div className="flex justify-center md:justify-start gap-4 fade-in-bottom-later">
+            <FloatingDockDemo></FloatingDockDemo>
+            {/* <div className="flex justify-center md:justify-start gap-4 fade-in-bottom-later">
+              
               <Button variant="outline" size="sm" className="gap-2 bigger-hover">
                 <FileText className="w-4 h-4" />
                 Resume
@@ -47,7 +50,7 @@ export default function Home() {
                 <Mail className="w-4 h-4" />
                 Contact
               </Button>
-            </div>
+            </div> */}
           </div>
         </div>
         <TabsDemo />
--- src/components/ui/TabsDemo.tsx
+++ src/components/ui/TabsDemo.tsx
@@ -57,7 +57,7 @@ export function TabsDemo() {
   ];
 
   return (
-    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
+    <div className="h-[20rem] md:h-[40rem] [perspective:500px]   ">
       <Tabs tabs={tabs} />
     </div>
   );
@@ -68,8 +68,8 @@ const DummyContent = () => {
     <img
       src="/linear.webp"
       alt="dummy image"
-      width="1000"
-      height="1000"
+      width="500"
+      height="500"
       className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
     />
   );
