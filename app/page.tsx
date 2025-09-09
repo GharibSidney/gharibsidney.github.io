9db99fc3ab09e8aab19fcebabdf89da0971ffae2
GharibSidney <sidney-gharib@hotmail.com>
2025-09-09 08:35:21 -0400

    removed fade-in that didnt work

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
     <div className="min-h-screen bg-background text-foreground dark background-container">
       <div className="container mx-auto px-4 py-8 max-w-4xl">
         {/* Hero Section */}
-        <div className="flex flex-col md:flex-row items-center gap-8 mb-2 fade-in-bottom">
+        <div className="flex flex-col md:flex-row items-center gap-8 mb-2">
           {/* Profile Image */}
-          <div className="flex-shrink-0 fade-in-bottom">
+          <div className="flex-shrink-0 ">
             <Avatar className="w-48 h-48 mb-2">
               <AvatarImage src={"/moi.jpg"} alt="Photo" />
               <AvatarFallback>SG</AvatarFallback>
