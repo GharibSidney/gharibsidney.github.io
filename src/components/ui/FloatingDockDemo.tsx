8811792f0ea0153ce9f2dc3418612aa1af6e4f54
GharibSidney <sidney-gharib@hotmail.com>
2025-09-09 09:16:49 -0400

    added link to email

--- src/components/ui/FloatingDockDemo.tsx
+++ src/components/ui/FloatingDockDemo.tsx
@@ -28,14 +28,14 @@ export function FloatingDockDemo() {
       icon: (
         <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: "#",
+      href: "https://www.linkedin.com/in/sidney-gharib-a89346229/",
     },
     {
       title: "Email",
       icon: (
         <IconAddressBook className="h-full w-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: "#",
+      href: "mailto:gharibsidney@gmail.com?subject=Hello Sidney&body=Hi, I found your website and wanted to reach out...",
     },
   ];
   return (
